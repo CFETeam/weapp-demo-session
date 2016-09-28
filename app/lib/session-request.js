00d4afa72e1f476548673a12d9bd4a259b870ddf
yomeeliu <yomeeliu@tencent.com>
2016-09-28 20:52:46 +0800

    update: `lib/session-request.js`

--- app/lib/session-request.js
+++ app/lib/session-request.js
@@ -1,15 +1,20 @@
 const co = require('./co.js');
 const promisify = require('./promisify.js');
 
-const constants = {
+const headers = {
     WX_CODE: 'X-WX-Code',
     WX_RAW_DATA: 'X-WX-RawData',
     WX_SIGNATURE: 'X-WX-Signature',
-
-    WX_SESSION_MAGIC_ID: 'F2C224D4-2BCE-4C64-AF9F-A6D872000D1A',
-
-    MAX_RETRY_TIMES: 3,
 };
+
+const errors = {
+    ERR_SESSION_EXPIRED: 'ERR_SESSION_EXPIRED',
+    ERR_SESSION_KEY_EXCHANGE_FAILED: 'ERR_SESSION_KEY_EXCHANGE_FAILED',
+    ERR_UNTRUSTED_RAW_DATA: 'ERR_UNTRUSTED_RAW_DATA',
+};
+
+const SESSION_MAGIC_ID = 'F2C224D4-2BCE-4C64-AF9F-A6D872000D1A';
+const MAX_RETRY_TIMES = 3;
 
 const login = promisify(wx.login);
 const getUserInfo = promisify(wx.getUserInfo);
@@ -24,7 +29,7 @@ let pendingHeader = null;
  */
 const buildHeader = co.wrap(function *() {
     if (currentCode) {
-        return { [constants.WX_CODE]: currentCode };
+        return { [headers.WX_CODE]: currentCode };
     } else {
         return pendingHeader = pendingHeader || co(function *() {
             const { code } = yield login();
@@ -34,9 +39,9 @@ const buildHeader = co.wrap(function *() {
             pendingHeader = null;
 
             return {
-                [constants.WX_CODE]: currentCode,
-                [constants.WX_RAW_DATA]: encodeURIComponent(rawData),
-                [constants.WX_SIGNATURE]: signature,
+                [headers.WX_CODE]: currentCode,
+                [headers.WX_RAW_DATA]: encodeURIComponent(rawData),
+                [headers.WX_SIGNATURE]: signature || '',
             };
         });
     }
@@ -59,10 +64,10 @@ function requestWithSession(options = {}) {
         const callFail = error => {
             fail && fail(error);
             complete && complete(error);
-            throw(error);
+            throw error;
         };
 
-        if (tryTimes++ > constants.MAX_RETRY_TIMES) {
+        if (tryTimes++ > MAX_RETRY_TIMES) {
             return callFail(new Error('请求失败次数过多'));
         }
 
@@ -70,17 +75,16 @@ function requestWithSession(options = {}) {
             header: Object.assign({}, options.headers, yield buildHeader()),
 
             success({ data }) {
-                if (data[constants.WX_SESSION_MAGIC_ID]) {
+                if (SESSION_MAGIC_ID in data) {
                     const error = data.error;
 
                     switch (data.reason) {
-                    case 'ERR_SESSION_EXPIRED':
-                    case 'ERR_SESSION_KEY_EXCHANGE_FAILED':
+                    case errors.ERR_SESSION_EXPIRED:
+                    case errors.ERR_SESSION_KEY_EXCHANGE_FAILED:
                         currentCode = null;
                         return wrapRequest();
 
-                    case 'ERR_UNTRUSTED_RAW_DATA':
-                    case 'ERR_JSCODE_NOT_PASSED':
+                    case errors.ERR_UNTRUSTED_RAW_DATA:
                     default:
                         return callFail(error);
                     }
